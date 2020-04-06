2b4b3828b94d513d17db19c56095bc55c492a9c1
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-04-06 16:34:04 +0200

    controladores funcionando

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -27,7 +27,52 @@ const TaskController = {
         })
     },
 
-    
+    taskCreate(req,res) {
+        let { nombre, descripcion} = req.body;
+        Task.create({
+            name,
+            descripcion
+        })
+        .then(data => {
+            res.statusCode = 201;
+            res.json(data);
+        })
+        .catch( err => {
+            res.statusCode = 500;
+            res.json(`"error": ${err}`);
+        })
+
+    },
+
+    taskUpdate(req,res) {
+         let { id } = req.params;
+         let { nombre, descripcion } = req.body;
+        
+         Task.update(
+             { nombre, descripcion },
+             { where: { id }}
+         )
+         .then(data => {
+             res.statusCode = 201;
+             res.json(data);
+         })
+         .catch( err => {
+            res.statusCode = 500;
+            res.json(`"error": ${err}`);
+        })
+    },
+
+    taskDelete(req,res) {
+        let { id } = req.params;
+        Task.destroy(
+            { where: { id }})
+        .then(data => {
+            res.statusCode = 200;
+            res.json(data);
+        })
+    }
+
+
 }
 
 module.exports = TaskController;
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const tasksController = require('../controllers/tasksController');
 
 
-/* GET users listing. */
+//Rutas. 
 router.get('/',tasksController.taskFindAll);
 router.get('/:id', tasksController.taskFindById);
-
-
-
+router.post('/', tasksController.taskCreate);
+router.put('/:id', tasksController.taskUpdate);
+router.delete('/:id', tasksController.taskDelete);
 
 module.exports = router;
